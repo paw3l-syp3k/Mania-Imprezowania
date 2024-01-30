b0bb433d93a86195e6f3b3410e92a2eb697a805b
Pawel sypekh <paw3lsyp3k@icloud.com>
2024-01-30 20:45:09 +0100

    adding mobile navigation

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,13 @@
 const hamburgerIcon = document.querySelector('.hamburger');
 const menuContainer = document.querySelector('.wrapper-1-header-menu');
+const menuItems = document.querySelectorAll('.wrapper-1-menu-item');
 
-hamburgerIcon.addEventListener('click', () => {
+const menuMobile = () => {
   hamburgerIcon.classList.toggle('active');
+  menuContainer.classList.toggle('show-menu-mobile');
+};
+
+hamburgerIcon.addEventListener('click', menuMobile);
+menuItems.forEach((item) => {
+  item.addEventListener('click', menuMobile);
 });
